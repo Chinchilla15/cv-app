da831448ceb9ab8173f2ffdb1614fef4e696c083
Alessandro Chinchilla <ale030401@gmail.com>
2024-06-05 11:07:20 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CV App *(Under Construction)*
+
+CV Generator App made with React. Part of The Odin Project - JavaScript Course 06/05/2024
---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import "../styles/App.css";
+
+function App() {
+	return (
+		<>
+			<h1>CV App Under Construction...</h1>
+			<h2>Please come back later!</h2>
+		</>
+	);
+}
+export default App;
