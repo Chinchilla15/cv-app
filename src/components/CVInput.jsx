bfd5e0db178c2ea69425549f28a3ddf48e24dc7a
Alessandro Chinchilla <ale030401@gmail.com>
2024-06-06 16:42:19 -0600

    Add Input component and info containers

--- src/components/CVInput.jsx
+++ src/components/CVInput.jsx
@@ -1,6 +1,43 @@
-import { useState } from "react";
 import "../styles/App.css";
+import { useState } from "react";
 
+function Input({ label, type, placeholder }) {
+	return (
+		<label>
+			{label}
+			<input type={type} placeholder={placeholder}></input>
+		</label>
+	);
+}
+function GeneralInfo() {
+	return (
+		<div className="generalInfo">
+			<Input label="Name" />
+			<Input label="Email" type="email" placeholder="example@mail.com" />
+			<Input label="Phone" type="tel" />
+		</div>
+	);
+}
+
+function EducationInfo() {
+	return (
+		<div className="educationInfo">
+			<Input label="Education " />
+		</div>
+	);
+}
+
+function ProfInfo() {
+	return (
+		<div className="profInfo">
+			<Input label="Professional" />
+		</div>
+	);
+}
 export default function CVInput() {
-	return <section className="cvInput">Input</section>;
+	return (
+		<section className="cvInput">
+			<GeneralInfo />
+		</section>
+	);
 }
