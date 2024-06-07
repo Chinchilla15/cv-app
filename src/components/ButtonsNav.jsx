76488b9c8336ec866c0b034740befe51888b7e4c
Alessandro Chinchilla <ale030401@gmail.com>
2024-06-07 15:51:28 -0600

    Refactor component rendering and added state to buttons. Add: import for components

--- src/components/ButtonsNav.jsx
+++ src/components/ButtonsNav.jsx
@@ -1,15 +1,16 @@
 import { motion } from "framer-motion";
 
-function Button({ text, iconName }) {
+function Button({ text, iconName, onClick }) {
 	return (
 		<motion.button
-			initial={{ opacity: 0.3 }}
+			initial={{ opacity: 0 }}
 			animate={{ opacity: 1, transition: { duration: 0.8 } }}
 			whileHover={{
 				scale: 1.1,
 				transition: { duration: 0.25 },
 			}}
 			whileTap={{ scale: 1 }}
+			onClick={onClick}
 		>
 			<i className={`fa-solid ${iconName}`}></i>
 			{text}
@@ -17,13 +18,29 @@ function Button({ text, iconName }) {
 	);
 }
 
-export default function ButtonsNav() {
+export default function ButtonsNav({ setActiveComponent }) {
 	return (
 		<div className="buttonsNav">
-			<Button text={"General Info"} iconName={"fa-user"} />
-			<Button text={"Education"} iconName={"fa-graduation-cap"} />
-			<Button text={"Work History"} iconName={"fa-briefcase"} />
-			<Button text={"Skills"} iconName={"fa-lightbulb"} />
+			<Button
+				text={"General Info"}
+				iconName={"fa-user"}
+				onClick={() => setActiveComponent("GeneralInfo")}
+			/>
+			<Button
+				text={"Education"}
+				iconName={"fa-graduation-cap"}
+				onClick={() => setActiveComponent("EducationInfo")}
+			/>
+			<Button
+				text={"Work History"}
+				iconName={"fa-briefcase"}
+				onClick={() => setActiveComponent("WorkHistory")}
+			/>
+			<Button
+				text={"Skills"}
+				iconName={"fa-lightbulb"}
+				onClick={() => setActiveComponent("Skills")}
+			/>
 		</div>
 	);
 }
--- src/components/CVInput.jsx
+++ src/components/CVInput.jsx
@@ -1,43 +1,23 @@
 import "../styles/App.css";
-import { useState } from "react";
+import GeneralInfo from "./forms/GeneralInfo";
+import EducationInfo from "./forms/Education";
+import WorkHistory from "./forms/WorkHistory";
+import Skills from "./forms/Skills";
 
-function Input({ label, type, placeholder }) {
-	return (
-		<label>
-			{label}
-			<input type={type} placeholder={placeholder}></input>
-		</label>
-	);
-}
-function GeneralInfo() {
-	return (
-		<div className="generalInfo">
-			<Input label="Name" />
-			<Input label="Email" type="email" placeholder="example@mail.com" />
-			<Input label="Phone" type="tel" />
-		</div>
-	);
-}
-
-function EducationInfo() {
-	return (
-		<div className="educationInfo">
-			<Input label="Education " />
-		</div>
-	);
-}
-
-function ProfInfo() {
-	return (
-		<div className="profInfo">
-			<Input label="Professional" />
-		</div>
-	);
-}
-export default function CVInput() {
-	return (
-		<section className="cvInput">
-			<GeneralInfo />
-		</section>
-	);
+export default function CVInput({ activeComponent }) {
+	const renderComponent = () => {
+		switch (activeComponent) {
+			case "GeneralInfo":
+				return <GeneralInfo />;
+			case "EducationInfo":
+				return <EducationInfo />;
+			case "WorkHistory":
+				return <WorkHistory />;
+			case "Skills":
+				return <Skills />;
+			default:
+				return <GeneralInfo />;
+		}
+	};
+	return <section className="cvInput">{renderComponent()}</section>;
 }
