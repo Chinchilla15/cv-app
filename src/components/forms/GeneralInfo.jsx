372c76012c3c23a0410fe475223a1242e793722b
Alessandro Chinchilla <ale030401@gmail.com>
2024-06-13 09:56:00 -0600

    add: state prop to input and general info, output displays info on change

--- src/components/forms/GeneralInfo.jsx
+++ src/components/forms/GeneralInfo.jsx
@@ -1,39 +1,73 @@
 import Input from "./Input";
 
-export default function GeneralInfo() {
+export default function GeneralInfo({ generalInfo, onGeneralInfoChange }) {
 	return (
 		<div className="generalInfo">
 			<div className="contactInfo">
-				<Input label="First Name" />
-				<Input label="Last Name" />
+				<Input
+					label="First Name"
+					value={generalInfo.firstName}
+					onChange={(value) =>
+						onGeneralInfoChange("firstName", value)
+					}
+				/>
+				<Input
+					label="Last Name"
+					value={generalInfo.lastName}
+					onChange={(value) => onGeneralInfoChange("lastName", value)}
+				/>
 				<Input
 					label="Email"
 					type="email"
 					placeholder="example@mail.com"
+					value={generalInfo.email}
+					onChange={(value) => onGeneralInfoChange("email", value)}
 				/>
-				<Input label="Phone" type="tel" />
+				<Input
+					label="Phone"
+					type="tel"
+					value={generalInfo.phone}
+					onChange={(value) => onGeneralInfoChange("phone", value)}
+				/>
 				<Input
 					label="Location"
 					type="text"
 					placeholder={"Country, City"}
+					value={generalInfo.location}
+					onChange={(value) => onGeneralInfoChange("location", value)}
 				/>
 				<Input
 					label="Occupation"
 					type="text"
 					placeholder={"E.g. Developer, Student..."}
+					value={generalInfo.occupation}
+					onChange={(value) =>
+						onGeneralInfoChange("occupation", value)
+					}
 				/>
 				<Input
 					label="Languages"
 					type="text"
 					placeholder={"English, French, German..."}
+					value={generalInfo.languages}
+					onChange={(value) =>
+						onGeneralInfoChange("languages", value)
+					}
 				/>
 				<Input
 					label="LinkedIn"
 					type="text"
 					placeholder={"Profile Link"}
+					value={generalInfo.linkedin}
+					onChange={(value) => onGeneralInfoChange("linkedin", value)}
 				/>
 			</div>
-			<Input label="Summary" placeholder="Tell me about yourself..." />
+			<Input
+				label="Summary"
+				placeholder="Tell me about yourself..."
+				value={generalInfo.summary}
+				onChange={(value) => onGeneralInfoChange("summary", value)}
+			/>
 		</div>
 	);
 }
--- src/components/forms/Input.jsx
+++ src/components/forms/Input.jsx
@@ -1,16 +1,25 @@
-export default function Input({ label, type, placeholder }) {
+export default function Input({ label, type, placeholder, value, onChange }) {
 	if (label === "Summary") {
 		return (
 			<label className="summaryInput">
 				{label}
-				<textarea placeholder={placeholder}></textarea>
+				<textarea
+					placeholder={placeholder}
+					value={value}
+					onChange={(e) => onChange(e.target.value)}
+				></textarea>
 			</label>
 		);
 	}
 	return (
 		<label>
 			{label}
-			<input type={type} placeholder={placeholder}></input>
+			<input
+				type={type}
+				placeholder={placeholder}
+				value={value}
+				onChange={(e) => onChange(e.target.value)}
+			></input>
 		</label>
 	);
 }
