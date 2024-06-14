07bda2ea54bb847d1880993e4e2db0517743e0ec
Alessandro Chinchilla <ale030401@gmail.com>
2024-06-14 15:32:18 -0600

    Add: save,edit,delete functionality; Entries boxes; state management

--- src/components/forms/Skills.jsx
+++ src/components/forms/Skills.jsx
@@ -1,15 +1,80 @@
+import { useState } from "react";
 import FormButtons from "../FormButton";
 import Input from "./Input";
 
-export default function Skills() {
+export default function Skills({ onSave, onDelete, onEdit, entries }) {
+	const [skills, setSkills] = useState({
+		category: "",
+		details: "",
+	});
+
+	const [editIndex, setEditIndex] = useState(null);
+
+	const handleChange = (field, value) => {
+		setSkills((prev) => ({
+			...prev,
+			[field]: value,
+		}));
+	};
+
+	const handleSave = () => {
+		if (editIndex !== null) {
+			onEdit(editIndex, skills);
+		} else {
+			onSave(skills);
+		}
+		setSkills({
+			category: "",
+			details: "",
+		});
+		setEditIndex(null);
+	};
+
+	const handleEditClick = (index) => {
+		setEditIndex(index);
+		setSkills(entries[index]);
+	};
+
+	const handleDeleteClick = (index) => {
+		onDelete(index);
+	};
+
 	return (
 		<div className="skillsInfo">
-			<Input label="Category" placeholder={"E.g. Soft Skills "} />
 			<Input
-				label="Skills details"
-				placeholder={"Disciplined, Organized, Responsible..."}
+				label={"Category"}
+				type={"text"}
+				value={skills.category}
+				placeholder={"E.g. Soft Skills "}
+				onChange={(value) => handleChange("category", value)}
 			/>
-			<FormButtons />
+			<Input
+				label={"Skills details"}
+				type={"text"}
+				value={skills.details}
+				placeholder={"Disciplined, Organized, Responsible..."}
+				onChange={(value) => handleChange("details", value)}
+			/>
+			<FormButtons onSave={handleSave} />
+			<div className="skillsEntries">
+				{entries.map((entry, index) => (
+					<div key={index} className="skillsEntryBox">
+						<p>{entry.category}</p>
+						<div className="editDeleteBox">
+							<button onClick={() => handleEditClick(index)}>
+								Edit
+							</button>
+							<button
+								onClick={() => {
+									handleDeleteClick(index);
+								}}
+							>
+								Delete
+							</button>
+						</div>
+					</div>
+				))}
+			</div>
 		</div>
 	);
 }
