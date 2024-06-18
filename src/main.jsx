d727f42bb08657b5438d74a0646e9aabc23c760a
Alessandro Chinchilla <ale030401@gmail.com>
2024-06-18 15:27:38 -0600

    Update: imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App.jsx";
-import "./styles/index.css";
+import App from "./App";
+import "./styles/Global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
