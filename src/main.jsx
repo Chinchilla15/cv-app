c0d5d58f72dd3b59c593bc4f1700f4efbb071ac2
Alessandro Chinchilla <ale030401@gmail.com>
2024-06-19 09:48:05 -0600

    Update dependencies. Add: Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/Global.css";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<Analytics />
-		<SpeedInsights />
 		<App />
 	</React.StrictMode>
 );
